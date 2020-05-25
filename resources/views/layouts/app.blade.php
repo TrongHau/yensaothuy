b6d6f579d2504413034ccff8896f5ba2e2f21c10
CSN\admin <tt.hau94@gmail.com>
2020-05-25 11:02:17 +0700

    no message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,8 +2,18 @@
 <html class="no-js" lang="vi" >
 
 @include('layouts.header')
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PNTWQTV');</script>
+<!-- End Google Tag Manager -->
 <body class="stBody stHome index">
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PNTWQTV"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <div id="insLoadpage" class="preloader">
     <div class="wrapLoading">
         <div class="loader">Loading...</div>
